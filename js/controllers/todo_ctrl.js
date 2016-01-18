16c55c2361534ffadd47c9f8d90cc1b6cb2a7f1e
Nano Molina <nanomolinacav@gmail.com>
2016-01-18 17:27:25 -0300

    List to do, agregar y limpiar items

--- js/controllers/todo_ctrl.js
+++ js/controllers/todo_ctrl.js
@@ -3,6 +3,35 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller.('TodoCtrl', function($scope) {
+myApp.controller('ListToDoCtrl', function(messages) {
+  var self = this;
+  self.messages = messages.list;
+  self.new_msg = 'Hello World!';
+  self.addItem = function(msg) {
+    messages.add(msg);
+  };
+  self.deleteItems = function() {
+    messages.clear();
+    self.messages = messages.list;
+  }
 
-})
+});
+
+// FACTORY
+myApp.factory('messages', function() {
+  var messages = {};
+  messages.list = [{done: false, text: 'prueba'}];
+  messages.add = function(msg) {
+    messages.list.push({done: false, text: msg});
+  }
+  messages.clear = function() {
+    var list_aux = messages.list;
+    messages.list = [];
+    angular.forEach(list_aux, function(todo) {
+      if (!todo.done) {
+        messages.list.push(todo);
+      }
+    });
+  }
+  return messages;
+});
