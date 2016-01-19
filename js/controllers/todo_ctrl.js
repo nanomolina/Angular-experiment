1c89b98b2d182f7d9ec0d40cbda5c0e729db7a14
Nano Molina <nanomolinacav@gmail.com>
2016-01-19 12:57:05 -0300

    se modifica msg de alrta

--- js/controllers/todo_ctrl.js
+++ js/controllers/todo_ctrl.js
@@ -7,12 +7,33 @@ myApp.controller('ListToDoCtrl', function(messages) {
   var self = this;
   self.messages = messages.list;
   self.new_msg = 'Hello World!';
+  self.items_deleted = 0;
+  self.items_added = 0;
+  self.alert_info = false;
+  self.info = '';
+  self.added = false;
+  self.deleted = false;
+
   self.addItem = function(msg) {
     messages.add(msg);
+    self.alert_info = true;
+    self.deleted = false;
+    self.added = true;
+    self.items_added += 1;
+    self.info = self.items_added + ' items added.';
   };
+
   self.deleteItems = function() {
+    var old_num_items = self.messages.length;
     messages.clear();
     self.messages = messages.list;
+    var new_num_items = self.messages.length;
+    self.items_added = 0;
+    self.items_deleted = old_num_items - new_num_items;
+    self.alert_info = old_num_items > new_num_items;
+    self.info = (self.alert_info ? self.items_deleted + ' items cleaned.' : '');
+    self.added = false;
+    self.deleted = self.alert_info;
   }
 
 });
@@ -21,9 +42,11 @@ myApp.controller('ListToDoCtrl', function(messages) {
 myApp.factory('messages', function() {
   var messages = {};
   messages.list = [{done: false, text: 'prueba'}];
+
   messages.add = function(msg) {
     messages.list.push({done: false, text: msg});
   }
+  
   messages.clear = function() {
     var list_aux = messages.list;
     messages.list = [];
@@ -35,3 +58,16 @@ myApp.factory('messages', function() {
   }
   return messages;
 });
+
+
+//ANIMATIONS
+myApp.animation('.fold-animation', ['$animateCss', function($animateCss) {
+  return {
+    enter: function(element) {
+      return $animateCss(element, {
+        easing: 'ease-out',
+        duration: 2 // two second
+      });
+    }
+  }
+}]);
